37d504e66f5cefa477a3022d34984ccef9e22c9d
paulxuca <paulxuca@gmail.com>
2019-04-24 22:27:45 -0400

    Idk man

--- lua/hello_world.lua
+++ lua/hello_world.lua
@@ -1,3 +1,19 @@
-local signed_message = ngx.req.get_headers()["Signed-Message"];
+local supported_nids = ec:get_curves_id()
 
-ngx.say("<p>" .. signed_message .. "</p>");
+local pub, err = ec:new({public_key = "0x31eC10983998b0c269160b80b8b08B5a72CA0190"})
+
+if not pub then
+    ngx.say('new ec err: ', err)
+    
+    return ngx.exit(500)
+end
+
+local flag, err = public:verify("Sign to start streaming.", sig)
+
+if err then
+    ngx.say('verify with ec err: ', err)
+
+    return ngx.exit(500)    
+end
+
+ngx.say(flag)
